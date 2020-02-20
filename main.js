ec435ef590ad4d3e2ba842e4d94007f6f667b93d
Jesús Iván Godínez Martínez <jgodinez0@ucol.mx>
2020-02-19 18:36:32 -0600

    Se añadio getFormato12 y getFormato24

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 import Fecha from "./fecha.js"
+import Tiempo from "./tiempo.js";
 
 class Main {
     probarFecha() {
-        let dia = prompt("Ingresa el dia")
-        let mes = prompt("Ingresa el mes")
-        let año = prompt("Ingresa el año")
-        let fecha = new Fecha(dia, mes, año);
-        console.log(`<---------Fecha--------->`)
+        console.log(`<---------Fecha--------->`);
+        //let dia = prompt("Ingresa el dia")
+        //let mes = prompt("Ingresa el mes")
+        //let año = prompt("Ingresa el año")
+        //let fecha = new Fecha(dia, mes, año);
+        let fecha = new Fecha(19, 2, 2000);
         console.log(`La fecha indicada es: ${fecha.getFecha()}`)
         console.log(`El día fue: ${fecha.getDiaFecha()}`)
         console.log(`Han pasado ${fecha.getAños()} años`);
@@ -15,7 +17,14 @@ class Main {
         console.log(`Han pasado ${fecha.getDias()} días`)
 
     }
+    probarTiempo() {
+        console.log(`<---------Tiempo--------->`)
+        let tiempo = new Tiempo(11, 11, "PM");
+        console.log(`La hora en el formato 24HRS es: ${tiempo.getFormato24()}`)
+        console.log(`La hora en el formato AM/PM es: ${tiempo.getFormato12()}`)
+    }
 }
 
 let app = new Main
 app.probarFecha();
+app.probarTiempo();
